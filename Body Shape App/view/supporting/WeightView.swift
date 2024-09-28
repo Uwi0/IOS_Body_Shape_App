c191110e8dc71be665fb40ccb657651938515f74
uwi0 <kakapoweb@gmail.com>
2024-09-28 13:50:29 +0700

    Weight view part 1

---
+++ Body Shape App/view/supporting/WeightView.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+
+struct WeightView: View {
+    let weight: CGFloat
+    let weightUnits: String
+    var body: some View {
+        HStack {
+            VStack {
+                HStack {
+                    Text("\(weight, specifier: "%.2f")")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    Text("\(weightUnits)")
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                }
+                Text("Current Weight")
+                    .font(.subheadline)
+                    .foregroundStyle(.gray)
+            }
+            VStack(alignment: .leading, spacing: 5) {
+                HStack {
+                    Text("2.5")
+                        .font(.title)
+                    Text(weightUnits)
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                }
+                Rectangle()
+                    .fill(.green)
+                    .frame(width: screenWidth / 2, height: 10)
+                Text("Left to Gain")
+                    .font(.subheadline)
+                    .foregroundStyle(.gray)
+            }
+        }
+    }
+}
+
+#Preview {
+    WeightView(weight: 61.2, weightUnits: "lb")
+}
