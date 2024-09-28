40a97821f42e65009a4595c0c736a9c4d9141cf2
uwi0 <kakapoweb@gmail.com>
2024-09-28 13:22:25 +0700

    Initial Commit

---
+++ Body Shape App/Body_Shape_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Body_Shape_AppApp.swift
+//  Body Shape App
+//
+//  Created by dwi prasetyo on 28/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Body_Shape_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
